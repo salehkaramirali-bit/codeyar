3bad6a25398e9ac639e31f2b347c9de71436d1aa
salehkaramirali-bit <salehkaramirali@gmail.com>
2026-09-09 19:39:34 +0330

    Update submit-homework.php

--- submit-homework.php
+++ submit-homework.php
@@ -2,7 +2,21 @@
 header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
 
+$name = $_POST['hwName'] ?? '';
+$email = $_POST['hwEmail'] ?? '';
+$course = $_POST['hwCourse'] ?? '';
+$description = $_POST['hwDescription'] ?? '';
+
+if (empty($name) || empty($description)) {
+    echo json_encode(['success' => false, 'message' => 'نام و توضیحات الزامی است']);
+    exit;
+}
+
 $dataFile = 'data/homework.json';
+
+if (!file_exists('data')) {
+    mkdir('data', 0777, true);
+}
 
 $homeworks = [];
 if (file_exists($dataFile)) {
@@ -15,8 +29,29 @@ if (file_exists($dataFile)) {
     }
 }
 
-echo json_encode([
-    'success' => true,
-    'homework' => $homeworks
-]);
+$homeworks[] = [
+    'id' => time(),
+    'name' => $name,
+    'email' => $email,
+    'course' => $course,
+    'description' => $description,
+    'date' => date('Y/m/d H:i:s'),
+    'status' => 'new'
+];
+
+file_put_contents($dataFile, json_encode($homeworks, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
+
+// ارسال ایمیل
+$to = 'salehkaramirali@gmail.com';
+$subject = 'تکلیف جدید از کدیار - ' . $course;
+$message = "📋 تکلیف جدید از کدیار\n\n";
+$message .= "👤 نام: $name\n";
+$message .= "📧 ایمیل: $email\n";
+$message .= "📚 دوره: $course\n";
+$message .= "📝 توضیحات:\n$description\n\n";
+$message .= "---\nارسال شده از کدیار 🧙‍♂️\n";
+
+mail($to, $subject, $message, "Content-Type: text/plain; charset=UTF-8");
+
+echo json_encode(['success' => true, 'message' => 'تکلیف با موفقیت ارسال شد! 📤']);
 ?>
